b92b0005a0f7330e181357b8a7c4bb54a193f519
Alexandr <81583071+Alexandr2106@users.noreply.github.com>
2022-05-03 19:10:11 +0300

    Add files via upload

---
+++ form/mailing-home.php
@@ -0,0 +1,14 @@
+<section class="mail">
+    <div class="container content">
+        <div class="mailing">
+            <h4>Подписывайтесь на нашу рассылку!</h4>
+            <p class="undt-p">И будьте в курсе свежих новостей каждую неделю</p>
+            <form action="" class="form-mailing">
+                <input type="email" class="form-control" placeholder="Ваш e-mail">
+
+                <button type="submit" class="btn">Подписаться</button>
+            </form>
+            <p class="footnote">Нажимая на кнопку, вы даете согласие на обработку персональных данных и политику конфиденциальности.</p>
+        </div>
+    </div>
+</section>
