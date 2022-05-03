546f6a17a20f9d16fc3edc593e53ec507f53ef93
Alexandr <81583071+Alexandr2106@users.noreply.github.com>
2022-05-03 19:10:28 +0300

    Add files via upload

---
+++ help/help.php
@@ -0,0 +1,138 @@
+<div class="container content-game">
+    <div class="layout">
+        <aside class="sidebar">
+            <nav class="sorts">
+                <div class="blok-header">
+                    Помощь
+                </div>
+                <ul class="sort">
+                    <li class="sort-item">
+                        <a href="#"><span class="filter sort-selected">Помощь</span></a>
+                    </li>
+                    <li class="sort-item">
+                        <a href="#"><span class="filter">О нас</span></a>
+                    </li>
+                    <li class="sort-item">
+                        <a href="#"><span class="filter">Реклама</span></a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+
+
+        <main class="help">
+            <section aria-label="breadcrumb" style="--bs-breadcrumb-divider: '|';">
+                <ol class="breadcrumb" style="margin: 0;">
+                    <li class="breadcrumb-item"><a href="./index.php">Главная</a></li>
+                    <li class="breadcrumb-item"><a href="#" class="disabled" style="text-decoration: none; cursor: auto;" disabled>Помощь</a></li>
+                </ol>
+            </section>
+
+            <div class="blok-header">Вопросы и ответы</div>
+
+            <div class="ask-question">
+                <div class="ask-button-item">
+                    <a href="#" class="btn">
+                        <span>Задать вопрос</span>
+                    </a>
+                </div>
+            </div>
+
+            <section>
+                <div class="help-list">
+                    <div class="help-items">
+                        <div class="help-item">
+                            <div class="question">
+                                <a href="#">Имеет ли смысл установка сжо на 12700kf, или хватит какого нибудь DARK ROCK PRO 4?...</a>
+                            </div>
+
+                            <div class="help-info">
+                                <div class="help-descript">
+                                    <span class="date" style="padding-right: 0;">2022-05-01 01:15:12</span>
+
+                                    <span class="views">
+                                        <img src="img/view-dark.png" class="imgi" alt="Просмотры">
+                                        1
+                                    </span>
+
+                                    <span class="comments">
+                                        <img src="img/chat-bubble-dark.png" class="imgi" alt="Комменты">
+                                        1
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div class="annotation">
+                                Собираю новый пк и встал такой вопрос — а хватит воздуха для этого процессора (пока без разгона, но в будущем планирую) или уже стоит смотреть в сторону воды...
+                            </div>
+
+                            <div class="status">
+                                <span class="status-open">Открыт <img src="/img/close.png" alt="Вопрос открыт"></span>
+                            </div>
+                        </div>
+
+                        <div class="help-item">
+                            <div class="question">
+                                <a href="#">Survival mars не запускается...</a>
+                            </div>
+
+                            <div class="help-info">
+                                <div class="help-descript">
+                                    <span class="date" style="padding-right: 0;">2022-05-01 01:15:12</span>
+
+                                    <span class="views">
+                                        <img src="img/view-dark.png" class="imgi" alt="Просмотры">
+                                        10
+                                    </span>
+
+                                    <span class="comments">
+                                        <img src="img/chat-bubble-dark.png" class="imgi" alt="Комменты">
+                                        3
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div class="annotation">
+                                survival mars не запускается..Здравствуйте. При запуске survival mars столкнулся с проблемой пиратка не запускается, вылезает белое окно и через 5 секунд все исчезает. доп по устанавливал, железо подходит, ос винда...
+                            </div>
+
+                            <div class="status">
+                                <span class="status-check">Закрыт <img src="/img/check.png" alt="Вопрос открыт"></span>
+                            </div>
+                        </div>
+
+                        <div class="help-item">
+                            <div class="question">
+                                <a href="#">Где сделать доски в FS 22?...</a>
+                            </div>
+
+                            <div class="help-info">
+                                <div class="help-descript">
+                                    <span class="date" style="padding-right: 0;">2022-05-01 01:15:12</span>
+
+                                    <span class="views">
+                                        <img src="img/view-dark.png" class="imgi" alt="Просмотры">
+                                        10
+                                    </span>
+
+                                    <span class="comments">
+                                        <img src="img/chat-bubble-dark.png" class="imgi" alt="Комменты">
+                                        3
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div class="annotation">
+                                Где сделать доски что бы делать из них мебель?...
+                            </div>
+
+                            <div class="status">
+                                <span class="status-open">Открыт <img src="/img/close.png" alt="Вопрос открыт"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+</div>
